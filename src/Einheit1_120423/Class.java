8198ca9780d92bcd58ab030b43cbb1f231aa4b10
eggerteresa <125272296+eggerteresa@users.noreply.github.com>
2023-04-12 16:04:27 +0200

    ProgrammierenII

---
+++ src/Einheit1_120423/Class.java
@@ -0,0 +1,74 @@
+package Einheit1_120423;
+
+public class Class {
+    private String name;
+    private int hours;
+    private int maxCapacity;
+    private int enrolledStudents;
+
+
+    public Class(String name, int hours, int maxCapacity, int enrolledStudents) {
+        this.name = name;
+        this.hours = hours;
+        this.maxCapacity = maxCapacity;
+        this.enrolledStudents = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(int enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+
+
+    public boolean maxCapacityReached () { // könnte man auch mit boolean machen
+boolean capfull= false;
+        if(enrolledStudents<maxCapacity){
+           // System.out.println("Es ist noch Platz");
+            capfull=false;
+        }
+        if (enrolledStudents>=maxCapacity) {
+          //  System.out.println("Der Kurs ist voll");
+            capfull=true;
+        }
+        return capfull;
+    }
+    public void enroll() {
+        if (maxCapacityReached()) {
+            System.out.println("Schon voll!");
+        } else {
+            System.out.println("Angemeldet");
+            enrolledStudents++;
+        }
+    }
+
+
+
+}
---
+++ src/Einheit1_120423/Student.java
@@ -0,0 +1,89 @@
+package Einheit1_120423;
+
+import java.util.Arrays;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Class[] classes;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.classes = new Class[3]; // Class Array mit 3 initialisieren
+    }
+
+    public void enroll(Class c) { // Ein Objekt als Parameter übergeben
+        if (extracted(c)) return; // steigt aus der Methode aus // fügt sich automatisch hier ein extracted methode
+
+           if (!c.maxCapacityReached()) {
+        if (classes[classes.length -1] != null) {
+            classes = increaseClassArray(); // Wenn kein Platz mehr ist, machen wir mehr Platz
+     c.enroll();
+      classes [classes.length -1] = c;
+        } else {
+            for (int i = 0; i< classes.length; i++) {
+                if (classes[i] == null) {
+                    c.enroll();
+                    classes[i] = c;
+                    System.out.println("Kurs angemeldet");
+                    break;
+                }
+            } }
+        }
+
+        if (!c.maxCapacityReached()) { // max capacity noch nicht erreicht!
+            for (int i = 0; i < classes.length; i++) {
+                if (classes[i] == null) {
+                    c.enroll();
+                    classes[i] = c; // neue Klasse c wird in den Array eingefügt
+                    System.out.println("Kurs angemeldet");
+                    break; // steigt hier aus und überspringt die nächsten zwei geschwungenen klammern
+                }
+            }
+        } else {
+            System.out.println(" Keine Anmeldung mehr möglich ");
+        }
+
+    }
+    // Wir machen mehr platz im ARray
+    private Class [] increaseClassArray() {
+        Class[] temp = new Class[classes.length +1];
+        for (int i = 0; i< classes.length; i++) {
+            temp[i] = classes [i];
+        }
+        return temp;
+    }
+
+
+
+// extract Method (Teil in Methode Markieren und extract Method:
+    private boolean extracted(Class c) {
+        for (int i = 0; i < classes.length; i++) { // Wir schauen, ob er nicht schon angemeldet ist
+            if (classes[i] != null) {
+                if (classes[i].getName().equals(c.getName())) {
+                    System.out.println(c.getName() + "Bereits angemeldet");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void printClasses() {
+        for (int i = 0; i < classes.length; i++) {
+            System.out.println(classes[i]);
+        }
+
+    }
+
+    // Methode, damit statt Ortangabe im Array auch String herauskommt
+    @Override
+    public String toString() {
+        return "Student {" +
+                "firstName ='" + firstName + '\'' +
+                ", lastName ='" + lastName + '\'' +
+                // ",  classes=" + Arrays.toString(classes) + // zeigt auch inhalt der Klasse an
+                '}';
+    }
+}
